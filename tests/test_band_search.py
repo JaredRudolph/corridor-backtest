7947a4896a4c352d7ae9175ab30af794acbf6189
Jared Rudolph <jkrudolph@outlook.com>
2026-04-19 12:48:58 -0400

    fix: update import path for search_band in test file

--- tests/test_band_search.py
+++ tests/test_band_search.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from allocator.band_search import search_band
+from corridor_backtest.band_search import search_band
 
 
 # ---------------------------------------------------------------------------
